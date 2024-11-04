58d55fceec7b0e41524ff7022cf023da5564de05
Rudraksh Joshi <u22ee054@eed.svnit.ac.in>
2024-11-05 00:01:05 +0530

    garbage

--- popup.js
+++ popup.js
@@ -17,8 +17,33 @@ function handler(){
     const tx={
         to:address,
         value: ethers.utils.parseEther(amount),
-    }
+    } 
+    var a=document.getElementById("link");
+    a.href="somelink url";
+    wallet.sendTransaction(tx).then((txObj) =>{
+        console.log("txHash",txObj.hash);
+        document.getElementById("center").style.display="none";
+        const a = document.getElementById("link");
+        a.href='';
+        document.getElementById("center").style.display="block";
+    });
+    
 }
-var a=document.getElementById("link");
-a.href="somelink url";
-wallet.sendTra
+
+document.addEventListener("DOMContentLoaded",function(){
+    document
+    .getElementById("center")
+    .addEventListener("click",checkBalance);
+});
+
+function checkBalance(){
+    document.getElementById("center").style.display="";
+
+    const provider = new  ethers.providers.JsonRpcProvider("");
+    coonst signer=provider.getSigner();
+    console.log(signer);
+    const address = document.getElementById("address").value;
+    provider.getBalance(address).then((balance) => {
+        const balanceInEth=ethers.utils.formatEther(balance);
+        document.getElementById("check_balance").innerText='your balance: ${balanceInEth} MATIC'
+    }
